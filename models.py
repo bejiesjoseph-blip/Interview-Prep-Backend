cac1b2432ab11f4e260fc8ead3b4a263c75e0ec6
Bejies <bejiesjoseph@gmail.com>
2026-07-01 11:22:39 +0530

    feat: Added register DB

---
+++ models.py
@@ -0,0 +1,52 @@
+from config import db, cursor
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+def create_tables():
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100),
+        email VARCHAR(150) UNIQUE,
+        password VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+
+
+create_tables()
+
+#Register User
+
+def register_user(name, email, password):
+
+    cursor.execute(
+        "SELECT id FROM users WHERE email=%s",
+        (email,)
+    )
+
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        return {
+            "success": False,
+            "message": "User already exists"
+        }
+
+    hashed_password = generate_password_hash(password)
+
+    cursor.execute("""
+    INSERT INTO users(name, email, password)
+    VALUES(%s, %s, %s)
+    """, (name, email, hashed_password))
+
+    db.commit()
+
+    return {
+        "success": True,
+        "message": "User registered successfully"
+    }
+
+    
